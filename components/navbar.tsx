2ace774b9b194d3d5057de993388f30ef8a57256
donperro05 <santianqr@gmail.com>
2023-10-24 00:14:55 -0500

    line-buttons-nav-user

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "@/styles/navbar.module.css";
-import {PiUserCircle} from "react-icons/pi"
+import { PiUserCircle } from "react-icons/pi";
 
 export default function Navbar() {
+  const [selectedButton, setSelectedButton] = useState<number | null>(null);
+
   return (
     <header className={styles.header}>
       <h1>BumperPass</h1>
-      <nav>
+      <nav className={styles.navbar}>
         <ul className={styles.listbuttons}>
-          <li >
-            <a href="#">Search Now!</a>
-          </li>
+          {["Search Now!", "About Us", "Services", "Contact"].map(
+            (text, index) => (
+              <li key={index}>
+                <div
+                  className={selectedButton === index ? styles.selected : ""}
+                  onClick={() => setSelectedButton(index)}
+                >
+                  <a href="#">{text}</a>
+                </div>
+              </li>
+            )
+          )}
           <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <PiUserCircle/>
+            <PiUserCircle size={25} color="#F25922"/>
           </li>
         </ul>
       </nav>
