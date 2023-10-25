38ef8f8e4c6fbeb11c38b16c885a968a3ffa4c8e
donperro05 <santianqr@gmail.com>
2023-10-24 22:25:47 -0500

    bplogo-header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,20 @@
 import React, { useState } from "react";
 import styles from "@/styles/navbar.module.css";
 import { PiUserCircle } from "react-icons/pi";
+import Image from "next/image";
+import logoBP from "@/public/logoBP.webp";
 
 export default function Navbar() {
   const [selectedButton, setSelectedButton] = useState<number | null>(null);
 
   return (
     <header className={styles.header}>
-      <h1>BumperPass</h1>
+      <Image
+        alt="logobp"
+        src={logoBP}
+        width={200}
+        height={50}
+      />
       <nav className={styles.navbar}>
         <ul className={styles.listbuttons}>
           {["Search Now!", "About Us", "Services", "Contact"].map(
