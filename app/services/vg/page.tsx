0ba20b996eb64a8ce58ebd1fb630c30b208b97d4
donperro05 <santianqr@gmail.com>
2024-01-13 00:20:57 -0500

    Refactor page and header components

---
+++ app/services/vg/page.tsx
@@ -0,0 +1,3 @@
+export default function VGPage() {
+  return <div>VG</div>;
+}
--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <header className="p-2 border-b-2 border-primary">
+    <header className="p-2 border-b-2 border-primary sticky top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex flex-1 justify-between max-w-6xl mx-auto">
         <Image alt="logo" src="/logo.png" width={150} height={300} />
         <Navbar />
