be23753c32da6256ab9519fee9b70aac56e9c7d1
donperro05 <santianqr@gmail.com>
2024-01-12 22:57:02 -0500

    Add footer and update header layout

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,11 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <header className="p-2 flex flex-1 justify-between max-w-6xl mx-auto">
-      <Image alt="logo" src="/logo.png" width={150} height={300} />
-      <Navbar />
+    <header className="p-2 border-b-2 border-primary">
+      <div className="flex flex-1 justify-between max-w-6xl mx-auto">
+        <Image alt="logo" src="/logo.png" width={150} height={300} />
+        <Navbar />
+      </div>
     </header>
   );
 }
