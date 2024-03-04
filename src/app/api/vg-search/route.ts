3121f6ecbb18a75fdbba62bfd8fc3a027ffa7acf
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-03-03 23:40:28 -0500

    Update User-Agent in vg-search route

--- src/app/api/vg-search/route.ts
+++ src/app/api/vg-search/route.ts
@@ -1,5 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getRandomUserAgent } from "@/lib/userAgents";
+
+const userAgent =
+  getRandomUserAgent() ??
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
 
 type Body = {
   plates: string[];
@@ -61,8 +66,7 @@ async function validPlate(
       headers: {
         Accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Uset-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "User-Agent": userAgent,
         "Sec-Fetch-Site": "same-origin",
         Referer: "https://www.dmv.ca.gov/wasapp/ipp2/processPers.do",
         Origin: "https://www.dmv.ca.gov",
